b006be344a7779e266cc3b11685bd6cb0789a137
Marco <marc.arena@studenti.unina.it>
2026-07-09 20:00:59 +0200

    Continuo caso d'uso creazione

--- src/main/java/Controller/ControllerSegnalazioni.java
+++ src/main/java/Controller/ControllerSegnalazioni.java
@@ -4,6 +4,7 @@ import Entity.Categoria;
 import Entity.GestoreSegnalazioni;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 //Façade
 public class ControllerSegnalazioni {
@@ -11,20 +12,28 @@ public class ControllerSegnalazioni {
     //Attributi
     private static String idSegnalazioneCorrente;
 
-
-
-    //Metodi
-    //TODO
-
     // Metodo per creare una segnalazione
-    public static final boolean creaSegnalazione(String titolo, String descrizione, String categoria, String posizione, String idCittadino, LocalDateTime data, String urlImmagine){
+    public static final boolean creaSegnalazione(String titolo, String descrizione, String categoria, String posizione, String data, String urlImmagine){
 
         // Prima di effettuare la chiamata al Façade dello strato Entity, convertiamo il valore di categoria
         Categoria categoriaEnum = Categoria.valueOf(categoria);
+        LocalDateTime localData;
+
+        if(data != null) {
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+            localData = LocalDateTime.parse(data, formatter);
+
+        }else{
+
+            localData = null;
+
+        }
+
+        String idCittadino = ControllerUtenti.getIdUtenteCoorrente();
 
         GestoreSegnalazioni gest = new GestoreSegnalazioni();
-
-        boolean esito = gest.inserisciSegnalazione(titolo, descrizione, categoriaEnum, posizione, idCittadino, data, urlImmagine);
+        boolean esito = gest.inserisciSegnalazione(titolo, descrizione, categoriaEnum, posizione, idCittadino, localData, urlImmagine);
 
         return esito;
 
