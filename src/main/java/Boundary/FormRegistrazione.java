e72b0d9eed89e0452ee3f238c75cc5892c6899e2
Marco <marc.arena@studenti.unina.it>
2026-07-09 14:40:56 +0200

    Impostazioni dei primi button per la gestione di registrazione e accesso

--- src/main/java/Boundary/FormRegistrazione.java
+++ src/main/java/Boundary/FormRegistrazione.java
@@ -1,4 +1,44 @@
 package Boundary;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class FormRegistrazione {
+
+    private JPanel contentPanel;
+    private JButton registratiButton;
+
+    public FormRegistrazione() {
+        registratiButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Registra();
+
+            }
+        });
+    }
+
+    public JFrame apriFormRegistrazione(){
+
+        JFrame frame = new JFrame();
+        frame.setTitle("RegistraFrame");
+        frame.setContentPane(contentPanel);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        return frame;
+    }
+
+    private void Registra(){
+
+        System.out.println("TODO Implementation register");
+
+    }
+
 }
