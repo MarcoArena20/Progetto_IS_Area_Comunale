17e889b54d10cd738fc51b1e6cd4cb609c831c41
alessandro-04byte <alessandrociorra04@gmail.com>
2026-07-09 16:57:12 +0200

    creazione gui visualizza

--- src/main/java/Boundary/FormVisualizzaSegnalazioni.java
+++ src/main/java/Boundary/FormVisualizzaSegnalazioni.java
@@ -1,9 +1,32 @@
 package Boundary;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FormVisualizzaSegnalazioni {
 
     private JPanel contentPanel;
+    private JScrollPane scroll;
+    private JButton visualizzaDettaglioButton;
+
+    // 1. Definiamo i nomi delle colonne iniziali
+    String[] colonneIniziali = {"Titolo", "Stato", "Data Inserimento"};
+
+    // 2. Creiamo il modello con queste colonne (e 0 righe iniziali)
+    DefaultTableModel tableModel = new DefaultTableModel(colonneIniziali, 0);
+
+    // 4. Colleghiamo il modello alla JTable
+    private JTable tabellaSegnalazioni = new JTable(tableModel);
+
+    public FormVisualizzaSegnalazioni() {
+        visualizzaDettaglioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
 }
