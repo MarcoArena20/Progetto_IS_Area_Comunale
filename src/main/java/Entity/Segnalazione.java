fefea4cd1710a1cff42525a63ffaf59186d26a64
Giuliano Izzo <giuliano.izzo05@gmail.com>
2026-07-09 16:44:38 +0200

    Implementata logicz StatoSegnalazione

--- src/main/java/Entity/Segnalazione.java
+++ src/main/java/Entity/Segnalazione.java
@@ -41,6 +41,7 @@ public class Segnalazione {
     public void setPosizione(String posizione) { this.posizione = posizione; }
     public String getIdCittadino() { return idCittadino; }
     public StatoSegnalazione getStato() { return stato; }
+    public void setStato(StatoSegnalazione newState) {this.stato = newState;}
     public LocalDateTime getData() { return data; }
     public void setData(LocalDateTime data) { this.data = data; }
     public String getUrlImmagine() { return urlImmagine; }
--- src/main/java/Entity/StatoInviata.java
+++ src/main/java/Entity/StatoInviata.java
@@ -2,16 +2,45 @@ package Entity;
 
 public class StatoInviata implements StatoSegnalazione {
 
+    //Costruttore
+    public StatoInviata() {}
+
     //Metodi
     @Override
-    public boolean aggiornaStato(boolean esito) {
-        //TODO
-        return true;
+    public void aggiornaStato(Segnalazione segnalazione, boolean esito) {
+        System.out.println("[StatoInviata] Invocato aggiornaStato con esito: " + esito);
+
+        if (esito == true) {
+            //Avanzo di stato..
+            System.out.println("[StatoInviata] Avanzamento di stato..");
+            segnalazione.setStato(new StatoPresaInCarico());
+        } else {
+            //Operazione non ha effetto
+            System.out.println("[StatoInviata] Aggiornamento non ha effetto..");
+        }
+
     }
 
     @Override
-    public String getStato() {
-        //TODO
-        return "";
+    public StatoInviata getStato() {
+        System.out.println("[StatoInviata] " + this.toString());
+        return this;
     }
+
+    @Override
+    public String toString() {
+        return "Stato: Inviata";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("[StatoInviata] MainTest avviato..");
+
+        StatoInviata statoTest = new StatoInviata();
+
+        System.out.println("[StatoInviata] getStato: "+ statoTest.getStato());
+        System.out.println("[StatoInviata] toString: "+ statoTest.toString());
+
+
+    }
+
 }
