68c6c14a38344d4f319af9a0f50897383edd07dc
Marco <marc.arena@studenti.unina.it>
2026-07-11 14:42:36 +0200

    Implementazione caso d'uso per la modifica di una segnalazione

--- src/main/java/Boundary/FormModificaSegnalazione.java
+++ src/main/java/Boundary/FormModificaSegnalazione.java
@@ -1,6 +1,14 @@
 package Boundary;
 
+import Controller.ControllerSegnalazioni;
+import Entity.Categoria;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class FormModificaSegnalazione {
 
@@ -18,6 +26,12 @@ public class FormModificaSegnalazione {
 
     public FormModificaSegnalazione(){
 
+        modificaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                modificaSegnalazione();
+            }
+        });
     }
 
     public JFrame apriModificaForm(String titolo, String descrizione, String categoria, String posizione, String data, String urlImmagine){
@@ -33,15 +47,114 @@ public class FormModificaSegnalazione {
         frame.setVisible(true);
 
         titoloField.setText(titolo);
-        descrizioneField.setText(titolo);
-        titoloField.setText(titolo);
-        posizioneField.setText(titolo);
-        dataField.setText(titolo);
-        immagineField.setText(titolo);
+        descrizioneField.setText(descrizione);
+        categoriaBox.setSelectedItem(categoria);
+        posizioneField.setText(posizione);
+        dataField.setText(data);
+        immagineField.setText(urlImmagine);
 
         modificaFrame = frame;
 
         return frame;
+
+    }
+
+    private boolean modificaSegnalazione(){
+
+        // Caratteristiche obbligatorie che ogni segnalazione deve avere
+        String titolo = titoloField.getText();
+        String descrizione = descrizioneField.getText();
+        String categoria = (String) categoriaBox.getSelectedItem();
+        String posizione = posizioneField.getText();
+
+        if(!verificaCampiObbligatori(titolo, descrizione, posizione, categoria)){
+
+            return false;
+
+        }
+
+        // Caratteristiche opzionali che una segnalazione può avere
+        String urlImmagine = immagineField.getText();
+        String data = dataField.getText();
+
+        if (urlImmagine.equalsIgnoreCase(""))
+            urlImmagine = null;
+
+        if(data.equalsIgnoreCase(""))
+            data = null;
+
+        if (!verificaCampiOpzionali(urlImmagine, data)){
+            return false;
+        }
+        else
+        {
+            boolean esito = ControllerSegnalazioni.modificaSegnalazione(titolo, descrizione, categoria, posizione, data, urlImmagine);
+            return esito;
+        }
+
+    }
+
+    private boolean verificaCampiObbligatori(String titolo, String descrizione, String posizione, String categoria){
+
+        if (titolo.length() < 5 || titolo.length() > 15){
+
+            System.out.println("Errore nel titolo");
+            return false;
+
+        }
+
+        if (descrizione.length() < 50 || descrizione.length() > 200) {
+
+            System.out.println("Errore nella descrizione");
+            return false;
+
+        }
+
+        if (posizione.length() < 10 || posizione.length() > 20){
+
+            System.out.println("Errore nella posizione");
+            return false;
+
+        }
+
+        try{
+
+            Categoria.valueOf(categoria);
+
+        }catch (IllegalArgumentException e){
+
+            System.out.println("Errore nella categoria");
+            return false;
+        }
+
+        return true;
+
+    }
+
+    private boolean verificaCampiOpzionali(String urlImmagine, String data){
+
+        if (urlImmagine != null && (urlImmagine.length() < 10 || urlImmagine.length() > 50)){
+
+            System.out.println("Errore nell'url");
+            return false;
+
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        try{
+
+            if(data != null)
+                LocalDateTime.parse(data, formatter);
+
+        }catch(DateTimeException e){
+
+            System.out.println("Errore nella data");
+            return false;
+
+        }
+
+        return true;
 
     }
 }
