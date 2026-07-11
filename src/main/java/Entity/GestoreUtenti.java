dbd9b8c6a38492d19ce7c96d2020efde2a2f70df
falconeantonio85 <falcone.antonio85@gmail.com>
2026-07-11 12:09:48 +0200

    Verifica che un email gia registrata con ruolo specificato nel form non possa registrarsi. Aggiunta una funzione privata in GestoreUtente verificaUtenteGiaRegistrato per verificare il check suddetto

--- src/main/java/Entity/GestoreUtenti.java
+++ src/main/java/Entity/GestoreUtenti.java
@@ -12,66 +12,97 @@ public class GestoreUtenti {
     //Attributi
     private final GestorePersistenza gestorePersistenza;
 
-    public GestoreUtenti(){
+    public GestoreUtenti() {
 
         this.gestorePersistenza = new GestorePersistenza();
 
     }
 
+    private boolean verificaUtenteGiaRegistrato(Ruolo ruolo, String email) {
+        if(ruolo == Ruolo.CITTADINO) {
+            return !gestorePersistenza.cercaPerCampo(Cittadino.class, "email", email).isEmpty();
+        } else {
+            return !gestorePersistenza.cercaPerCampo(Operatore.class, "email", email).isEmpty();
+        }
+    }
+
     //Metodi
-    public String registraUtente(Ruolo ruolo, String nome, String cognome, String email, String recapitoTelefonico, String passwordHash){
-        if (gestorePersistenza.cercaPerCampo(UtenteAutenticato.class, "email", email)==null){
+    public String registraUtente(Ruolo ruolo, String nome, String cognome, String email, String recapitoTelefonico, String passwordHash) {
+        //verifico che non ci sia un Utente(Cittadino/Operatore) che si sta registrando con un email gia registrata per il ruolo scelto
+        if (verificaUtenteGiaRegistrato(ruolo, email)) {
             return null;
-        }
-        else if(ruolo == Ruolo.OPERATORE){
-            Operatore operatore = new Operatore(nome,cognome,email,recapitoTelefonico,passwordHash);
-            gestorePersistenza.salva(operatore);
-            return operatore.getIdOperatore().toString();
-        }
-        else{
-            Cittadino cittadino = new Cittadino(nome,cognome,email,recapitoTelefonico,passwordHash);
-            gestorePersistenza.salva(cittadino);
-            return cittadino.getIdCittadino().toString();
-        }
-    }
-    public String accessoUtente(Ruolo ruolo, String email, String passwordHash){
-        if (ruolo == Ruolo.OPERATORE){
-            Operatore operatoreAccesso = (Operatore) cercaUtente(email, passwordHash);
-            if (operatoreAccesso != null){
-                return  operatoreAccesso.getIdOperatore().toString();
+        } else {
+            //Assicurati che non esista un utente con email e ruolo specificati gia registrati
+            //procedo alla registrazione e salvataggio dell'utente
+            if (ruolo == Ruolo.CITTADINO) {
+                Cittadino cittadino = new Cittadino(nome, cognome, email, recapitoTelefonico, passwordHash);
+                gestorePersistenza.salva(cittadino);
+                return cittadino.getIdCittadino().toString();
+            } else {
+                Operatore operatore = new Operatore(nome, cognome, email, recapitoTelefonico, passwordHash);
+                gestorePersistenza.salva(operatore);
+                return operatore.getIdOperatore().toString();
             }
-            else {return null;}
-        }
-        else{
-            Cittadino cittadinoAccesso = (Cittadino) cercaUtente(email, passwordHash);
-            if (cittadinoAccesso != null){
-                return  cittadinoAccesso.getIdCittadino().toString();
-            }
-            else {return null;}
         }
     }
 
-    public UtenteAutenticato cercaUtente(String email, String passwordHash) {
+    public String accessoUtente(Ruolo ruolo, String email, String passwordHash) {
+        if (ruolo == Ruolo.OPERATORE) {
+            Operatore operatoreAccesso = cercaUtenteOperatore(email, passwordHash);
+            if (operatoreAccesso != null) {
+                return operatoreAccesso.getIdOperatore().toString();
+            } else {
+                return null;
+            }
+        } else {
+            Cittadino cittadinoAccesso = cercaUtenteCittadino(email, passwordHash);
+            if (cittadinoAccesso != null) {
+                return cittadinoAccesso.getIdCittadino().toString();
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public Cittadino cercaUtenteCittadino(String email, String passwordHash) {
         //creo i criteri per cercare nel DB(email e pass)
-        Map<String, Object> criteriRicercaUtente = new HashMap<>();
+        Map<String, Object> criteriRicercaUtenteCittadino = new HashMap<>();
 
-        criteriRicercaUtente.put("email", email);
-        criteriRicercaUtente.put("password", passwordHash);
+        criteriRicercaUtenteCittadino.put("email", email);
+        criteriRicercaUtenteCittadino.put("passwordHash", passwordHash);
 
         //faccio una richiesta al gestore della persistenza
-        List<UtenteAutenticato> listaUtentiTrovati;
-        listaUtentiTrovati = gestorePersistenza.cercaPerCampi( UtenteAutenticato.class ,criteriRicercaUtente);
-        //verifico che sia stato effettivamente trovato un UtenteAutenticato con quell'email e pass
-        if(listaUtentiTrovati!= null && !listaUtentiTrovati.isEmpty()){
-            return  listaUtentiTrovati.get(0);
+        List<Cittadino> listaUtentiTrovati;
+        listaUtentiTrovati = gestorePersistenza.cercaPerCampi(Cittadino.class, criteriRicercaUtenteCittadino);
+        //verifico che sia stato effettivamente trovato un Cittadino con quell'email e pass
+        if (listaUtentiTrovati != null && !listaUtentiTrovati.isEmpty()) {
+            return listaUtentiTrovati.get(0);
+        } else {
+            return null;
         }
-        else{return null;}
+    }
+
+    public Operatore cercaUtenteOperatore(String email, String passwordHash) {
+        //creo i criteri per cercare nel DB(email e pass)
+        Map<String, Object> criteriRicercaUtenteOperatore = new HashMap<>();
+
+        criteriRicercaUtenteOperatore.put("email", email);
+        criteriRicercaUtenteOperatore.put("passwordHash", passwordHash);
+
+        //faccio una richiesta al gestore della persistenza
+        List<Operatore> listaUtentiTrovati;
+        listaUtentiTrovati = gestorePersistenza.cercaPerCampi(Operatore.class, criteriRicercaUtenteOperatore);
+        //verifico che sia stato effettivamente trovato un Cittadino con quell'email e pass
+        if (listaUtentiTrovati != null && !listaUtentiTrovati.isEmpty()) {
+            return listaUtentiTrovati.get(0);
+        } else {
+            return null;
+        }
     }
 
     public Cittadino cercaUtente(Long idCittadino) {
 
         return gestorePersistenza.trovaPerId(Cittadino.class, idCittadino);
     }
-
-
 }
+
