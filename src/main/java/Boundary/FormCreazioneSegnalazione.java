18d0e430ecf1b9c6c37996b146e13e10ad851ba0
Marco <marc.arena@studenti.unina.it>
2026-07-12 11:56:31 +0200

    Modularizzazione codice modifica e creazione segnalazione

--- src/main/java/Boundary/FormCreazioneSegnalazione.java
+++ src/main/java/Boundary/FormCreazioneSegnalazione.java
@@ -35,7 +35,7 @@ public class FormCreazioneSegnalazione {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                Map<String, String> dati = recuperaDatiSegnalazione();
+                Map<String, String> dati = CheckFormSegnalazione.recuperaDatiSegnalazione(titoloField, descrizioneField, categoriaBox, posizioneComboBox, dataField, urlImmagineField);
 
                 boolean creata = creaSegnalazione(dati.get("titolo"),
                                                 dati.get("descrizione"),
@@ -70,49 +70,9 @@ public class FormCreazioneSegnalazione {
 
     }
 
-    private Map<String, String> recuperaDatiSegnalazione(){
-
-        // Caratteristiche obbligatorie che ogni segnalazione deve avere
-        String titolo = titoloField.getText();
-        String descrizione = descrizioneField.getText();
-        String categoria = (String) categoriaBox.getSelectedItem();
-        String posizione = (String) posizioneComboBox.getSelectedItem();
-
-        // Caratteristiche opzionali che una segnalazione può avere
-        String urlImmagine = urlImmagineField.getText();
-        String data = dataField.getText();
-
-        Map<String, String> dati = new HashMap<>();
-        dati.put("titolo", titolo);
-        dati.put("descrizione", descrizione);
-        dati.put("categoria", categoria);
-        dati.put("posizione", posizione);
-        dati.put("data", data);
-        dati.put("urlImmagine", urlImmagine);
-
-        return dati;
-
-    }
-
     public boolean creaSegnalazione(String titolo, String descrizione, String categoria, String posizione, String data, String urlImmagine){
 
-        if(!CheckFormSegnalazione.checkTitolo(titolo))
-            return false;
-
-        if(!CheckFormSegnalazione.checkDescrizione(descrizione))
-            return false;
-
-        if(!CheckFormSegnalazione.checkCategoria(categoria))
-            return false;
-
-        if(!CheckFormSegnalazione.checkPosizione(posizione))
-            return false;
-
-        if(!CheckFormSegnalazione.checkData(data))
-            return false;
-
-        if(!CheckFormSegnalazione.checkUrlImmagine(urlImmagine))
-            return false;
+        CheckFormSegnalazione.checkDatiSegnalazione(titolo, descrizione, categoria, posizione, data, urlImmagine);
 
         if (urlImmagine.equalsIgnoreCase(""))
             urlImmagine = null;
--- src/main/java/Boundary/FormModificaSegnalazione.java
+++ src/main/java/Boundary/FormModificaSegnalazione.java
@@ -1,14 +1,11 @@
 package Boundary;
 
 import Controller.ControllerSegnalazioni;
-import Entity.Enum.Categoria;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.time.DateTimeException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 public class FormModificaSegnalazione {
 
@@ -17,12 +14,12 @@ public class FormModificaSegnalazione {
     private JPanel labelPanel;
     private JPanel fieldPanel;
     private JTextField dataField;
-    private JTextField immagineField;
-    private JTextField posizioneField;
+    private JTextField urlImmagineField;
     private JTextField descrizioneField;
     private JTextField titoloField;
     private JButton modificaButton;
     private JComboBox categoriaBox;
+    private JComboBox posizioneBox;
 
     public FormModificaSegnalazione(int idRow){
 
@@ -30,9 +27,17 @@ public class FormModificaSegnalazione {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                boolean esito = modificaSegnalazione(idRow);
+                Map<String, String> dati = CheckFormSegnalazione.recuperaDatiSegnalazione(titoloField, descrizioneField, categoriaBox, posizioneBox, dataField, urlImmagineField);
 
-                if(esito){
+                boolean modificata = modificaSegnalazione(idRow,
+                        dati.get("titolo"),
+                        dati.get("descrizione"),
+                        dati.get("categoria"),
+                        dati.get("posizione"),
+                        dati.get("data"),
+                        dati.get("urlImmagine"));
+
+                if(modificata){
 
                     modificaFrame.dispose();
                     new FormVisualizzaSegnalazioni().apriFormVisualizzaSegnalazioni();
@@ -58,9 +63,9 @@ public class FormModificaSegnalazione {
         titoloField.setText(titolo);
         descrizioneField.setText(descrizione);
         categoriaBox.setSelectedItem(categoria);
-        posizioneField.setText(posizione);
+        posizioneBox.setSelectedItem(posizione);
         dataField.setText(data);
-        immagineField.setText(urlImmagine);
+        urlImmagineField.setText(urlImmagine);
 
         modificaFrame = frame;
 
@@ -68,23 +73,9 @@ public class FormModificaSegnalazione {
 
     }
 
-    private boolean modificaSegnalazione(int idRow){
+    private boolean modificaSegnalazione(int idRow, String titolo, String descrizione, String categoria, String posizione, String data, String urlImmagine){
 
-        // Caratteristiche obbligatorie che ogni segnalazione deve avere
-        String titolo = titoloField.getText();
-        String descrizione = descrizioneField.getText();
-        String categoria = (String) categoriaBox.getSelectedItem();
-        String posizione = posizioneField.getText();
-
-        if(!verificaCampiObbligatori(titolo, descrizione, posizione, categoria)){
-
-            return false;
-
-        }
-
-        // Caratteristiche opzionali che una segnalazione può avere
-        String urlImmagine = immagineField.getText();
-        String data = dataField.getText();
+        CheckFormSegnalazione.checkDatiSegnalazione(titolo, descrizione, categoria, posizione, data, urlImmagine);
 
         if (urlImmagine.equalsIgnoreCase(""))
             urlImmagine = null;
@@ -92,78 +83,7 @@ public class FormModificaSegnalazione {
         if(data.equalsIgnoreCase(""))
             data = null;
 
-        if (!verificaCampiOpzionali(urlImmagine, data)){
-            return false;
-        }
-        else
-        {
-            boolean esito = ControllerSegnalazioni.modificaSegnalazione(idRow, titolo, descrizione, categoria, posizione, data, urlImmagine);
-            return esito;
-        }
-
-    }
-
-    private boolean verificaCampiObbligatori(String titolo, String descrizione, String posizione, String categoria){
-
-        if (titolo.length() < 5 || titolo.length() > 15){
-
-            System.out.println("Errore nel titolo");
-            return false;
-
-        }
-
-        if (descrizione.length() < 50 || descrizione.length() > 200) {
-
-            System.out.println("Errore nella descrizione");
-            return false;
-
-        }
-
-        if (posizione.length() < 10 || posizione.length() > 20){
-
-            System.out.println("Errore nella posizione");
-            return false;
-
-        }
-
-        try{
-
-            Categoria.valueOf(categoria);
-
-        }catch (IllegalArgumentException e){
-
-            System.out.println("Errore nella categoria");
-            return false;
-        }
-
-        return true;
-
-    }
-
-    private boolean verificaCampiOpzionali(String urlImmagine, String data){
-
-        if (urlImmagine != null && (urlImmagine.length() < 10 || urlImmagine.length() > 50)){
-
-            System.out.println("Errore nell'url");
-            return false;
-
-        }
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-
-        try{
-
-            if(data != null)
-                LocalDateTime.parse(data, formatter);
-
-        }catch(DateTimeException e){
-
-            System.out.println("Errore nella data");
-            return false;
-
-        }
-
-        return true;
+        return ControllerSegnalazioni.modificaSegnalazione(idRow, titolo, descrizione, categoria, posizione, data, urlImmagine);
 
     }
 }
