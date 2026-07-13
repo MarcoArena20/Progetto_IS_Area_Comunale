ffab72c7beae1b25ca70ac89f6995eda5f8abee8
Giuliano Izzo <giuliano.izzo05@gmail.com>
2026-07-13 14:33:57 +0200

    Commentato (javadoc) codice di CreazioneNotaTest

--- src/test/java/CreazioneNotaTest.java
+++ src/test/java/CreazioneNotaTest.java
@@ -3,7 +3,10 @@ import Boundary.FormCreazioneSegnalazione;
 
 import Boundary.FormRegistrazione;
 import Controller.ControllerSegnalazioni;
+import Controller.ControllerUtenti;
 import Database.GestorePersistenza;
+import Entity.Enum.Ruolo;
+import Entity.Operatore;
 import Entity.Segnalazione;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,13 +18,34 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * CASO D'USO: aggiungiNotaInterna
+ * Classe di test per verificare il corretto funzionamento del caso d'uso aggiungiNotaInterna
+ */
+
 public class CreazioneNotaTest {
 
+    /**
+     * Attributo per far riferimento al form corrispondente
+     */
+
     private FormConclusioneGestione form;
+
+    /**
+     * Attributo per indicare la riga che si sta visualizzando al dettaglio (e quindi la segnalazione corrispondente
+     */
+
     private static Integer idRow = 0;
 
     //Il db si presuppone vuoto in fase di testing => si crea un operatore, un cittadino e una segnalazione (il cui id sarà 1);
     //poiché ci sarà una segnalazione, anche l'idRow sarà 1
+
+    /**
+     * Metodo utilizzato per
+     * 1. inizializzare il db con un cittadino, un operatore e una segnalaione;
+     * 2. ricavare id segnalazione e operatore
+     * 3. settare idUtente e idSegnalazioneCorrente
+     */
 
     @BeforeAll
     public static void setUp() {
@@ -43,21 +67,24 @@ public class CreazioneNotaTest {
 
         Long idSegnalazioneCorrente = listaS.get(0).getIdSegnalazione();
 
-        ControllerSegnalazioni.setIdSegnalazioneCorrente(idSegnalazioneCorrente);
+        List<Operatore> listaO = new GestorePersistenza().cercaPerCampi(Operatore.class, Map.of());
+        Long idOperatore = listaO.get(0).getIdOperatore();
 
+
+        ControllerSegnalazioni.setIdSegnalazioneCorrente(idSegnalazioneCorrente);
+        ControllerUtenti.setIdUtenteCorrente(idOperatore, Ruolo.OPERATORE.name());
     }
+
+    /**
+     * Metodo utilizzato per far prendere in carico adll'operatore la segnalazione
+     * Si nota che nel caso in cui venga stampato su terminale:
+     *[GestoreSegnalazioni] Impossibile prendere in carico la segnalazione, stato:PRESA_IN_CARICO
+     * è da considerarsi normale flusso di esecuzione visto che, in caso di errore in conclusione con aggiunta nota,
+     * la conclusione della gestione non va a buon fine e quindi l'operatore starà ancora gestendo quella operazione
+     */
 
     @BeforeEach
     public void prendiInCaricoSegnalazione() {
-        /*Metodo necessario poiché la precondizione per terminare una gestione e averla in carico;
-         * Si nota che nel caso in cui venga stampato su terminale:
-         *
-         *[GestoreSegnalazioni] Impossibile prendere in carico la segnalazione, stato:PRESA_IN_CARICO
-         *
-         * è da considerarsi normale flusso di esecuzione visto che, in caso di errore in conclusione con aggiunta nota,
-         * la conclusione della gestione non va a buon fine e quindi l'operatore starà ancora gestendo quella operazione
-         */
-
         //Il boundary non fa altro che chiamare questa funzione al premere del button prendi in carico;
         // -> di conseguenza il test parte dal controller
         ControllerSegnalazioni.iniziaGestioneSegnalazione();
@@ -73,6 +100,10 @@ public class CreazioneNotaTest {
      * IllegalArgumentException; di conseguenza, se non viene lanciata alcuna eccezione la conclusione è da considerarsi
      * andata a buon fine (anche se l'esito di quel metodo fosse false) visto che non sarebbero riscontrati problemi
      * nei valori inseriti come titolo e descrizione della nota
+     */
+
+    /**
+     * TC: ID1
      */
 
     @Test
@@ -99,6 +130,10 @@ public class CreazioneNotaTest {
         assertTrue(esito);
     }
 
+    /**
+     * TC: ID2
+     */
+
     @Test
     public void titoloLungo() {
         form.setTitoloNota("La Segnalazione è stata risolta");
@@ -121,6 +156,10 @@ public class CreazioneNotaTest {
 
         assertFalse(esito);
     }
+
+    /**
+     * TC: ID3
+     */
 
     @Test
     public void titoloCorto() {
@@ -145,6 +184,10 @@ public class CreazioneNotaTest {
         assertFalse(esito);
     }
 
+    /**
+     * TC: ID4
+     */
+
     @Test
     public void titoloSpeciale() {
         form.setTitoloNota("Ris@lta");
@@ -167,6 +210,10 @@ public class CreazioneNotaTest {
 
         assertFalse(esito);
     }
+
+    /**
+     * TC: ID5
+     */
 
     @Test
     public void descrizioneLunga() {
@@ -191,6 +238,10 @@ public class CreazioneNotaTest {
         assertFalse(esito);
     }
 
+    /**
+     * TC: ID6
+     */
+
     @Test
     public void descrizioneCorta() {
         form.setTitoloNota("Risolta");
@@ -213,6 +264,10 @@ public class CreazioneNotaTest {
 
         assertFalse(esito);
     }
+
+    /**
+     * TC: ID7
+     */
 
     @Test
     public void descrizioneSpeciale() {
